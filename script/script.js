3ee776f3ce5730cb6a78c960c7e7023a26c8c0b3
Ogrebalschick <ilya.ya.2011@bk.ru>
2021-10-16 22:11:58 +0600

    сделал кнопку закрытия footer, добавли картинки и текст. Сделал некоторые доработки

--- script/script.js
+++ script/script.js
@@ -8,14 +8,19 @@ function displ(ddd) {
         document.getElementById(ddd + '-link').innerText = 'Ещё';
     }
 }
-// type = "text/javascript" >
-//     function displ(ddd) {
-//         var el = document.getElementById(ddd);
-//         if (el.className.indexOf('hidden') >= 0) {
-//             el.className = 'text-box'; document.getElementById(ddd + '-link').innerText = 'Скрыть текст';
-//         }
-//         else {
-//             el.className = 'text-box hidden';
-//             document.getElementById(ddd + '-link').innerText = 'Ещё';
-//         }
-//     }
+
+
+// let btn = docuemnt.querySelector('#buttonid');
+// btn.addEventListener('clcik', (x) => {
+//     x.ClassList.toggle('show');
+// });
+
+    let btn = document.querySelector('#close');
+    let footer = document.querySelector('.footer__sitemap-wrapper');
+    btn.addEventListener('click', () => {footer.classList.remove('show')});
+
+
+
+
+    let fsw__btn = document.querySelector('#fsw__btn');
+    fsw__btn.addEventListener('click', () => {footer.classList.toggle('show')});
